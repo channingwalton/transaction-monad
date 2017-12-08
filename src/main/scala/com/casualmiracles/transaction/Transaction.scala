4fd1af28af089de1b5dc40e890aca9aaa2f3c41e
Channing Walton <channingwalton@mac.com>
2017-12-08 10:24:19 +0000

    inline things - not sure if it improves things or not

--- src/main/scala/com/casualmiracles/transaction/Transaction.scala
+++ src/main/scala/com/casualmiracles/transaction/Transaction.scala
@@ -3,7 +3,10 @@ package com.casualmiracles.transaction
 import cats.Monad
 
 
-final case class  Transaction[F[_] : Monad, E, A](run: F[(Either[E, A], PostCommit, PostCommit)]) {
+final case class Transaction[F[_] : Monad, E, A](run: F[(Either[E, A], PostCommit, PostCommit)]) {
+
+  type Run[T] = (Either[E, T], PostCommit, PostCommit)
+
   private val monadF = implicitly[Monad[F]]
 
   def postSuccess(f: () ⇒ Unit): Transaction[F, E, A] =
@@ -15,27 +18,17 @@ final case class  Transaction[F[_] : Monad, E, A](run: F[(Either[E, A], PostComm
   def map[B](f: A ⇒ B): Transaction[F, E, B] =
     Transaction(monadF.map(run)(v ⇒ (v._1.map(f), v._2, v._3)))
 
-  def flatMap[B](f: A ⇒ Transaction[F, E, B]): Transaction[F, E, B] = {
-
-    def runFunc(r: (Either[E, A], PostCommit, PostCommit)): F[(Either[E, B], PostCommit, PostCommit)] = {
-      def handRight(t: Transaction[F, E, B]): F[(Either[E, B], PostCommit, PostCommit)] = {
-        monadF.map(t.run) { runRight ⇒
-          (runRight._1, r._2 ::: runRight._2, r._3 ::: runRight._3)
+  def flatMap[B](f: A ⇒ Transaction[F, E, B]): Transaction[F, E, B] =
+    Transaction {
+      monadF.flatMap(run) {
+        r: Run[A] ⇒ {
+          r._1.map(f).fold(
+            (l: E) ⇒ monadF.pure((Left(l), r._2, r._3)),
+            (t: Transaction[F, E, B]) ⇒ monadF.map(t.run) { runRight: Run[B] ⇒
+              (runRight._1, r._2 ::: runRight._2, r._3 ::: runRight._3)
+            }
+          )
         }
       }
-
-      def handleLeft(l: E): F[(Either[E, B], PostCommit, PostCommit)] = {
-        monadF.pure((Left(l), r._2, r._3))
-      }
-
-      val newTrans: Either[E, Transaction[F, E, B]] = r._1.map(f)
-
-      newTrans.fold(handleLeft, handRight)
     }
-
-    val newRun: F[(Either[E, B], PostCommit, PostCommit)] =
-      monadF.flatMap(run)(runFunc)
-
-    Transaction(newRun)
-  }
 }
