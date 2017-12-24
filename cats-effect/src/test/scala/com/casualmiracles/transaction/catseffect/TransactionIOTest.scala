50a33be02787adbb455df27f4353b852d11e0194
Channing Walton <channingwalton@mac.com>
2017-12-24 18:27:10 +0000

    add tests for TransactionIO

--- cats-effect/src/test/scala/com/casualmiracles/transaction/catseffect/TransactionIOTest.scala
+++ cats-effect/src/test/scala/com/casualmiracles/transaction/catseffect/TransactionIOTest.scala
@@ -13,5 +13,10 @@ class TransactionIOTest extends FreeSpec with MustMatchers with EitherValues {
     "when the result is a failure" in {
       TransactionIO.failure[String, String]("oops").unsafeAttemptRun().left.value.right.value mustBe "oops"
     }
+
+    "when a non-fatal exception is thrown" in {
+      val exception = new RuntimeException()
+      TransactionIO.const[String, String]("ok").map(_ ⇒ throw exception).unsafeAttemptRun().left.value.left.value mustBe exception
+    }
   }
 }
