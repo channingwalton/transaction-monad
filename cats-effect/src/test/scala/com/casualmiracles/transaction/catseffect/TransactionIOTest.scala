59be9df4ea0c6ac2d280e674cba694f52f310825
Channing Walton <channingwalton@mac.com>
2017-12-28 21:14:17 +0000

    refactor tests

--- cats-effect/src/test/scala/com/casualmiracles/transaction/catseffect/TransactionIOTest.scala
+++ cats-effect/src/test/scala/com/casualmiracles/transaction/catseffect/TransactionIOTest.scala
@@ -8,44 +8,32 @@ import com.casualmiracles.transaction.Transaction
 class TransactionIOTest extends FreeSpec with MustMatchers with EitherValues {
 
   "Success" in {
-    var onSuccess = false
-    var onFailure = false
-    val trans = TransactionIO.success[String, String]("hi").onSuccess(() ⇒ onSuccess = true).onFailure(() ⇒ onFailure = true)
-
-    onSuccess mustBe false
-    onFailure mustBe false
-
-    trans.unsafeAttemptRun() mustBe RunResult.Success("hi")
-
-    onSuccess mustBe true
-    onFailure mustBe false
+    val trans = TransactionIO.success[String, String]("hi")
+    assertTransaction(trans, RunResult.Success[String, String]("hi"), true, false)
   }
 
   "Failure" - {
     "when the result is a failure" in {
-      var onSuccess = false
-      var onFailure = false
-
-      val trans = TransactionIO.failure[String, String]("oops").onSuccess(() ⇒ onSuccess = true).onFailure(() ⇒ onFailure = true)
-
-      trans.unsafeAttemptRun() mustBe RunResult.Failure("oops")
-
-      onSuccess mustBe false
-      onFailure mustBe true
+      val trans = TransactionIO.failure[String, String]("oops")
+      assertTransaction(trans, RunResult.Failure[String, String]("oops"), false, true)
     }
 
     "when a non-fatal exception is thrown" in {
       val exception = new RuntimeException()
-
-      var onSuccess = false
-      var onFailure = false
-
-      val trans: Transaction[IO, String, String] = TransactionIO.success[String, String]("ok").onSuccess(() ⇒ onSuccess = true).onFailure(() ⇒ onFailure = true).map(_ ⇒ throw exception)
-
-      trans.unsafeAttemptRun() mustBe RunResult.Error(exception)
-
-      onSuccess mustBe false
-      onFailure mustBe false
+      val trans: Transaction[IO, String, String] = TransactionIO.success[String, String]("ok").map(_ ⇒ throw exception)
+      assertTransaction(trans, RunResult.Error(exception), false, false)
     }
+  }
+
+  private def assertTransaction[E, A](trans: Transaction[IO, E, A], result: RunResult[E, A], onSuccessRun: Boolean, onFailureRun: Boolean) = {
+    var onSuccess = false
+    var onFailure = false
+
+    val transWithFunctions = trans.onSuccess(() ⇒ onSuccess = true).onFailure(() ⇒ onFailure = true)
+
+    transWithFunctions.unsafeAttemptRun() mustBe result
+
+    onSuccess mustBe onSuccessRun
+    onFailure mustBe onFailureRun
   }
 }
