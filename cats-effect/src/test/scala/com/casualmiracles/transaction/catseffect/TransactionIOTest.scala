e8a956885645d1bf686d9c339fd743ec492c97fc
Channing Walton <channingwalton@mac.com>
2017-12-28 21:06:10 +0000

    add tests

--- cats-effect/src/test/scala/com/casualmiracles/transaction/catseffect/TransactionIOTest.scala
+++ cats-effect/src/test/scala/com/casualmiracles/transaction/catseffect/TransactionIOTest.scala
@@ -2,21 +2,50 @@ package com.casualmiracles.transaction.catseffect
 
 import org.scalatest.{EitherValues, FreeSpec, MustMatchers}
 import TransactionIOOps._
+import cats.effect.IO
+import com.casualmiracles.transaction.Transaction
 
 class TransactionIOTest extends FreeSpec with MustMatchers with EitherValues {
 
   "Success" in {
-    TransactionIO.success[String, String]("hi").unsafeAttemptRun() mustBe RunResult.Success("hi")
+    var onSuccess = false
+    var onFailure = false
+    val trans = TransactionIO.success[String, String]("hi").onSuccess(() ⇒ onSuccess = true).onFailure(() ⇒ onFailure = true)
+
+    onSuccess mustBe false
+    onFailure mustBe false
+
+    trans.unsafeAttemptRun() mustBe RunResult.Success("hi")
+
+    onSuccess mustBe true
+    onFailure mustBe false
   }
 
   "Failure" - {
     "when the result is a failure" in {
-      TransactionIO.failure[String, String]("oops").unsafeAttemptRun() mustBe RunResult.Failure("oops")
+      var onSuccess = false
+      var onFailure = false
+
+      val trans = TransactionIO.failure[String, String]("oops").onSuccess(() ⇒ onSuccess = true).onFailure(() ⇒ onFailure = true)
+
+      trans.unsafeAttemptRun() mustBe RunResult.Failure("oops")
+
+      onSuccess mustBe false
+      onFailure mustBe true
     }
 
     "when a non-fatal exception is thrown" in {
       val exception = new RuntimeException()
-      TransactionIO.success[String, String]("ok").map(_ ⇒ throw exception).unsafeAttemptRun() mustBe RunResult.Error(exception)
+
+      var onSuccess = false
+      var onFailure = false
+
+      val trans: Transaction[IO, String, String] = TransactionIO.success[String, String]("ok").onSuccess(() ⇒ onSuccess = true).onFailure(() ⇒ onFailure = true).map(_ ⇒ throw exception)
+
+      trans.unsafeAttemptRun() mustBe RunResult.Error(exception)
+
+      onSuccess mustBe false
+      onFailure mustBe false
     }
   }
 }
