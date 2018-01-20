106901b3dc431d0d965107a55a3fe4c5d5d26a65
Channing Walton <channingwalton@mac.com>
2018-01-20 23:57:13 +0000

    add docs

--- core/src/main/scala/com/casualmiracles/transaction/package.scala
+++ core/src/main/scala/com/casualmiracles/transaction/package.scala
@@ -14,43 +14,81 @@ package object transaction {
   implicit val listMonoid: Monoid[List[String]] = cats.instances.all.catsKernelStdMonoidForList
 
   type TransactionStateF[F[_], A] = ReaderWriterStateT[F, List[String], List[String], List[PostRun], A]
-  type TransactionF[F[_], E, A]   = EitherT[TransactionStateF[F, ?], E, A]
 
+  /**
+    * The Transaction type
+    * @tparam F the effect
+    * @tparam E the error type
+    * @tparam A the value type
+    */
+  type TransactionF[F[_], E, A] = EitherT[TransactionStateF[F, ?], E, A]
+
+  /**
+    * A convenient type that describes the final result after the effect has been discharged.
+    */
   type RunResult[E, A] = (List[String], List[PostRun], Either[E, A])
 
+  /**
+    * An alias for successF.
+    */
   def pointF[F[_]: Applicative, E, A](a: A): TransactionF[F, E, A] =
     successF(a)
 
+  /**
+    * Construct a successful TransactionF. The value will be on the Right.
+    */
   def successF[F[_]: Applicative, E, A](a: A): TransactionF[F, E, A] =
     liftEitherF(Right[E, A](a))
 
+  /**
+    * Construct a failed TransactionF. The value will be on the Right.
+    */
   def failureF[F[_]: Applicative, E, A](e: E): TransactionF[F, E, A] =
     liftEitherF(Left[E, A](e))
 
+  /**
+    * Construct a TransactionF with an Either.
+    */
   def liftEitherF[F[_], E, A](e: Either[E, A])(implicit monadF: Applicative[F]): TransactionF[F, E, A] =
     applyF(ReaderWriterStateT[F, List[String], List[String], List[PostRun], Either[E, A]]((c, p) ⇒ monadF.point((c, p, e))))
 
-  def applyF[F[_], E, A](s: ReaderWriterStateT[F, List[String], List[String], List[PostRun], Either[E, A]]): TransactionF[F, E, A] =
+  private def applyF[F[_], E, A](s: ReaderWriterStateT[F, List[String], List[String], List[PostRun], Either[E, A]]): TransactionF[F, E, A] =
     EitherT(s)
 
+  /**
+    * Construct a TransactionF from an Option.
+    * If a Some it will be a successful TransactionF, if None it will be an unsuccessful TransactionF with the provided error.
+    */
   def liftOptionF[F[_]: Applicative, E, A](option: Option[A], error: E): TransactionF[F, E, A] =
     liftEitherF(option.fold[Either[E, A]](Left(error))(Right(_)))
 
+  /**
+    * Construct a successful TransactionF with a value in an effect F.
+    */
   def liftF[F[_], E, A](s: F[A])(implicit monadF: Applicative[F]): TransactionF[F, E, A] = applyF {
     ReaderWriterStateT[F, List[String], List[String], List[PostRun], Either[E, A]] {
       case (_, st) => monadF.map(s)(v ⇒ (List.empty[String], st, Right[E, A](v)))
     }
   }
 
+  /**
+    * Construct a TransactionF with a log value.
+    */
   def logF[F[_]: Applicative, E, A](msg: String): TransactionF[F, E, Unit] = liftSF {
     ReaderWriterStateT[F, List[String], List[String], List[PostRun], Unit] {
       case (_, s) => (List(msg), s, ()).pure[F]
     }
   }
 
+  /**
+    * Construct a TransactionF with a post run value.
+    */
   def postRunF[F[_]: Applicative, E, A](f: () ⇒ Unit): TransactionF[F, E, Unit] =
     postRunF[F, E, A](PostRun(f))
 
+  /**
+    * Construct a TransactionF with a post run value.
+    */
   def postRunF[F[_]: Applicative, E, A](pc: PostRun): TransactionF[F, E, Unit] =
     liftSF {
       ReaderWriterStateT[F, List[String], List[String], List[PostRun], Unit] {
@@ -61,6 +99,9 @@ package object transaction {
   private def liftSF[F[_]: Functor, E, A](s: ReaderWriterStateT[F, List[String], List[String], List[PostRun], A]): TransactionF[F, E, A] =
     EitherT.liftF(s)
 
+  /**
+    * Syntax for working with TransactionF instances.
+    */
   implicit class TransactionSyntax[F[_]: Monad, E, A](transaction: TransactionF[F, E, A]) {
 
     def postRun(f: () ⇒ Unit): TransactionF[F, E, A] =
